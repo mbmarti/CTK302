909a1318cca3fa2923832f822c7c113564a11f29
mbmarti <54522940+mbmarti@users.noreply.github.com>
2020-09-13 23:09:03 -0500

    Add files via upload

---
+++ p5/Unit1/sound/sketch.js
@@ -0,0 +1,24 @@
+var s;
+
+function preload() {
+  soundFormats(".mp3");
+  s = loadSound("assets/DarkAmbient.mp3");
+}
+
+function setup() {
+  createCanvas(800, 800);
+  s.loop();
+}
+
+function draw() {
+  background(100);
+}
+
+function mouseReleased() {
+  if (s.isPlaying()) {
+    s.pause();
+  }
+  else {
+    s.loop();
+  }
+}
