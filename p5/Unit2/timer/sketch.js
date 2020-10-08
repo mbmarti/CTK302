3821513c17736366e44d22ebfc8cae068f63dafe
mbmarti <54522940+mbmarti@users.noreply.github.com>
2020-10-08 16:05:37 -0500

    Add files via upload

---
+++ p5/Unit2/timer/sketch.js
@@ -0,0 +1,25 @@
+let timer = 0;
+let state = 0;
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  switch (state) {
+    case 0:
+    background('red');
+    text("case 0", 250, 250);
+    timer++;
+    if (timer > 200) {
+      state = 1;
+      timer = 0;
+    }
+    break;
+
+    case 1:
+    background('blue');
+    text("case 1", 250, 250);
+    break;
+  }
+}
