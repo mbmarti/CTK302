5438c1caef16b851ed4e434cd11e5de61e835a4d
mbmarti <54522940+mbmarti@users.noreply.github.com>
2020-10-08 22:23:17 -0500

    Add files via upload

--- p5/Unit2/mp2/sketch.js
+++ p5/Unit2/mp2/sketch.js
@@ -1,6 +1,8 @@
 let state = 0;
 let mic;
 let vol = 0;
+let timer = 0;
+let countdown = 5;
 
 function setup() {
   createCanvas(500, 500);
@@ -14,21 +16,43 @@ function draw() {
   switch (state) {
     case 0:
     background('red');
-    text("state 0", 250, 250);
+    text("yell at this guy", 250, 250);
+    vol = mic.getLevel();
+    if (vol >= 0.2) {
+      state++
+    }
     break;
 
     case 1:
     background('green');
-    text("either click or make noise", 250, 250);
+    text("uh oh, you've been detected! shhhhh\n" + countdown, 250, 250);
     vol = mic.getLevel();
     if (vol >= 0.1) {
       state++;
+    }
+    timer++;
+    if (timer > 1*60) {
+      countdown = 4;
+    }
+    if (timer > 2*60) {
+      countdown = 3;
+    }
+    if (timer > 3*60) {
+      countdown = 2;
+    }
+    if (timer > 4*60) {
+      countdown = 1;
+    }
+    if (timer > 5*60) {
+      state = 0;
+      timer = 0;
+      countdown = 5;
     }
     break;
 
     case 2:
     background('blue');
-    text("state 2", 250, 250);
+    text("oh wow you got caught, click to reload", 250, 250);
     break;
   }
 }
