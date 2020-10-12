00a8f086a4db2b17ad144adf75897c2c397a92c6
mbmarti <54522940+mbmarti@users.noreply.github.com>
2020-10-11 23:44:44 -0500

    Add files via upload

---
+++ p5/Unit2/c2/sketch.js
@@ -0,0 +1,36 @@
+var mic;
+var vol;
+let state = 0;
+let timer = 0;
+
+function setup() {
+  createCanvas(500, 500);
+
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw() {
+  background(100);
+
+  vol = (mic.getLevel()).toFixed(2);
+
+  switch(state);
+    case 0:
+    background('red');
+    text("shhh!!!", 250, 250);
+    if (vol > 0.10) {
+      case = 1;
+    }
+    break;
+
+    case 1:
+    background('green');
+    text("QUIET DOWN!", 250, 250);
+    timer++;
+    if (timer > 3*60) {
+      case = 0;
+      timer = 0;
+    }
+    break;
+}
