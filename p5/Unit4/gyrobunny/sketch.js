3b5a7626c6e3064c32432d0a5a65990dc05af9ed
mbmarti <54522940+mbmarti@users.noreply.github.com>
2020-12-09 17:57:38 -0600

    Add files via upload

--- p5/Unit4/gyrobunny/sketch.js
+++ p5/Unit4/gyrobunny/sketch.js
@@ -27,7 +27,7 @@ function draw() {
   // takes your variable and maps it from range 1 to range 2
   // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
   xPosition = map(gamma, -60, 60, 0, width);
-  yPosition = map(beta, -30, 90, 0, height);
+  yPosition = map(beta, -30, 30, 0, height);
 
   push(); // before you use translate, rotate, or scale commands, push and then pop after
 
@@ -41,27 +41,27 @@ function draw() {
 
 
   // Text commands that display debugging data
-  textAlign(LEFT);
-  textSize(20);
-  fill('black');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y.toFixed(2), 25, 170);
-  text("z = " + z.toFixed(4), 25, 190);
+  // textAlign(LEFT);
+  // textSize(20);
+  // fill('black');
+  // text("orientation data:", 25, 25);
+  // textSize(15);
+  // text("alpha: " + alpha, 25, 50);
+  // text("beta: " + beta, 25, 70);
+  // text("gamma: " + gamma, 25, 90);
+  // textSize(20);
+  // text("acceleration data:", 25, 125);
+  // textSize(15);
+  // text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  // text("y = " + y.toFixed(2), 25, 170);
+  // text("z = " + z.toFixed(4), 25, 190);
 
   // Text that makes CTK type in the background
-  fill('white');
+  fill('pink');
   noStroke();
-  textSize(300);
+  textSize(100);
   textAlign(CENTER);
-  text("ctk", width / 2, height / 2);
+  text("are you chain punk or egg punk?", width / 2, height / 2);
 
 }
 
